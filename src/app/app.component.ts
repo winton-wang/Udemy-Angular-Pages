4fd1e6e03a8074c3b81a14448dc9e463bd353013
Winton Wang <winton168@hotmail.com>
2020-06-22 10:50:57 -0400

    Bootstrap Custom Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,139 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+ 
+  currentPage = 0;
+
+  images = [
+    {
+      title : 'Beach Surfing',
+      url: 'https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Beach Surfboard',
+      url: 'https://images.unsplash.com/photo-1488402024618-0658e4c2b1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Sea',
+      url: 'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Bikini',
+      url: 'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title : 'Beach Surfing',
+      url: 'https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Beach Surfboard',
+      url: 'https://images.unsplash.com/photo-1488402024618-0658e4c2b1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Sea',
+      url: 'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Bikini',
+      url: 'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    
+    {
+      title : 'Beach Surfing',
+      url: 'https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Beach Surfboard',
+      url: 'https://images.unsplash.com/photo-1488402024618-0658e4c2b1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Sea',
+      url: 'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Bikini',
+      url: 'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    
+    {
+      title : 'Beach Surfing',
+      url: 'https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Beach Surfboard',
+      url: 'https://images.unsplash.com/photo-1488402024618-0658e4c2b1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Sea',
+      url: 'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Bikini',
+      url: 'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title : 'Beach Surfing',
+      url: 'https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Beach Surfboard',
+      url: 'https://images.unsplash.com/photo-1488402024618-0658e4c2b1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Sea',
+      url: 'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Bikini',
+      url: 'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    
+    {
+      title : 'Beach Surfing',
+      url: 'https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Beach Surfboard',
+      url: 'https://images.unsplash.com/photo-1488402024618-0658e4c2b1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Sea',
+      url: 'https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+      title : 'Bikini',
+      url: 'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }
+    
+    
+  ];
+
+
+
+  checkWindowIndex(index: number){
+
+    return Math.abs(this.currentPage - index) < 5;
+  }
+
+
+
+
 }
---
+++ src/app/class.directive.ts
@@ -0,0 +1,28 @@
+import { async } from '@angular/core/testing';
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]'
+})
+export class ClassDirective {
+ 
+
+  constructor(private element: ElementRef) { 
+
+  }
+
+  @Input('appClass') set className(classObj: any){
+     for(let key in classObj){
+
+       if ( classObj[key]){
+          this.element.nativeElement.classList.add(key);
+       } else {
+         this.element.nativeElement.classList.remove(key);
+       }
+
+        
+     }
+  }
+
+
+}
